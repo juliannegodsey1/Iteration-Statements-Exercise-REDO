6cfeda97c9f6ab2b22b42878c97ed9d760cbd379
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-28 16:23:28 -0500

    redo complete

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,33 +1,148 @@
-﻿namespace IterationStatements
+﻿using System.ComponentModel.Design;
+
+namespace IterationStatements
 {
     public class Program
     {
-        //LukeWarm Section: Create methods below
-        //Write a method that will print to the console all numbers 1000 through - 1000
-
-        //Write a method that will print to the console numbers 3 through 999 by 3 each time
-
-        //Write a method to accept two integers as parameterss and check whether they are equal or not
         
-        //Write a method to check whether a given number is even or odd
-        
-        //Write a method to check whether a given number is positive or negative
-        
-        //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
-        //Parse()
-        //TryParse()
-
-        //Heatin Up Section:
-        //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
-        //Write a method to display the multiplication table(from 1 to 12) of a given integer
-
-
-        //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Numbers 1000 to -1000:");
+            PrintThousand();
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Numbers 3 to 999, counted by 3:");
+            ByThrees();
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Check to see if two numbers are equal:");
+            Console.WriteLine("Please give one number:");
+            var userNumber1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please give a second number:");
+            var userNumber2 = int.Parse(Console.ReadLine());
+            IsEqual(userNumber1, userNumber2);
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Please give a number to see if it is even or odd:");
+            var userNum = int.Parse(Console.ReadLine());
+            IsEven(userNum);
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please give a number to see if it is positive or negative:");
+            var userInput = int.Parse(Console.ReadLine());
+            IsPositive(userInput);
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Are you old enough to vote? Enter your age to check!");
+            var userAge = int.Parse(Console.ReadLine());
+            CanVote(userAge);
+            Console.WriteLine("");
+            Console.WriteLine("------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Provide a number to see if it is in range between -10 and 10:");
+            var userCheckRange = int.Parse(Console.ReadLine());
+            InRange(userCheckRange);
+        }
+
+        public static void PrintThousand()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void ByThrees()
+        {
+            for (int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        public static void IsEqual(int x, int y)
+        {
+            if (x == y)
+            {
+                Console.WriteLine("These numbers are equal!");
+            }
+            else
+            {
+                Console.WriteLine("These numbers are NOT equal!");
+            }
+        }
+
+        public static void IsEven(int a)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"{a} is an EVEN number!");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is an ODD number!");
+            }
+
+        }
+
+        public static void IsPositive(int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine($"{b} is not a positive OR negative number! How neat!");
+            }
+            else if (b < 0)
+            {
+                Console.WriteLine($"{b} is a NEGATIVE number!");
+            }
+            else
+            {
+                Console.WriteLine($"{b} is a POSITIVE number!");
+
+            }
+
+        }
+
+        public static void CanVote(int k)
+        {
+            if (k >= 18)
+            {
+                Console.WriteLine($"Congrats! {k} years old is old enough to vote!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry! {k} is not old enough to vote yet.");
+            }
+
+        }
+
+
+        public static void InRange(int j)
+        {
+            if (j >= -10 && j <= 10)
+            {
+                Console.WriteLine($"The number you gave, {j} is in range!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {j} is NOT in range!");
+            }
+
         }
     }
 }
